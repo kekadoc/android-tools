2f6cff270baf730b8a732b04f9bcbaa23cba7fdd
qeGame <QeGame69@gmail.com>
2021-03-01 21:41:46 +0500

    01.03.2021 Drawing fix

--- androidtools/src/main/java/com/kekadoc/tools/android/graph/drawing/DrawingCircle.kt
+++ androidtools/src/main/java/com/kekadoc/tools/android/graph/drawing/DrawingCircle.kt
@@ -5,20 +5,25 @@ import android.graphics.Paint
 import android.graphics.RectF
 import com.kekadoc.tools.android.graph.drawing.Drawing
 
-class DrawingCircle(var bounds: RectF, var paint: Paint) : Drawing() {
+open class DrawingCircle(var bounds: RectF = RectF(), var paint: Paint) : Drawing() {
 
     var startAngle = 270f
     var sweepAngle = 360f
+    var useCenter = false
 
     fun setStartAngleFraction(fraction: Float) {
         startAngle = 360f * fraction
     }
     fun setSweepAngleFraction(fraction: Float) {
-        sweepAngle *= 360f * fraction
+        sweepAngle = 360f * fraction
     }
 
     override fun draw(canvas: Canvas) {
-        canvas.drawArc(bounds, startAngle, sweepAngle, false, paint)
+        canvas.drawArc(bounds, startAngle, sweepAngle, useCenter, paint)
+    }
+
+    override fun toString(): String {
+        return "DrawingCircle(bounds=$bounds, paint=$paint, startAngle=$startAngle, sweepAngle=$sweepAngle)"
     }
 
 }
